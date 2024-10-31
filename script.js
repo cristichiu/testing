7b191c3a1f829c5cf05a0bb419fedbcee01ff236
Cristian <83159231+cristichiu@users.noreply.github.com>
2024-10-31 10:14:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.addEventListener("click", function(event) {
-      if (event.target.classList.contains("main-header__action")) {
-          event.preventDefault();
-          fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
-      }
+document.getElementsByClassName("main-header__action")[0].addEventListener("click", function(event) {
+      event.preventDefault();
+      fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
 });
