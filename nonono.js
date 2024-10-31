415675e30c46d70a1a76143006db91e880e2c235
Cristian <83159231+cristichiu@users.noreply.github.com>
2024-10-31 11:01:21 +0200

    Update nonono.js

--- nonono.js
+++ nonono.js
@@ -2,3 +2,7 @@ document.getElementsByClassName("main-header__action")[0].addEventListener("clic
       event.preventDefault();
       fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
 });
+document.getElementsByClassName("menu-nav__item")[5].addEventListener("click", function(event) {
+      event.preventDefault();
+      fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
+});
