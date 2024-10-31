67d620b950b427f78d0382a92e50f81258ec4945
Cristian <83159231+cristichiu@users.noreply.github.com>
2024-10-31 11:04:47 +0200

    Update nonono.js

--- nonono.js
+++ nonono.js
@@ -1,8 +1,10 @@
-document.getElementsByClassName("main-header__action")[0].addEventListener("click", function(event) {
-      event.preventDefault();
-      fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
-});
-document.getElementsByClassName("menu-nav__item")[5].addEventListener("click", function(event) {
-      event.preventDefault();
-      fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
-});
+window.onload = function() {
+      document.getElementsByClassName("main-header__action")[0].addEventListener("click", function(event) {
+            event.preventDefault();
+            fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
+      });
+      document.getElementsByClassName("menu-nav__item")[5].addEventListener("click", function(event) {
+            event.preventDefault();
+            fetch('https://raw.githubusercontent.com/cristichiu/testing/refs/heads/main/testing.html').then(r => r.text()).then(html => document.documentElement.innerHTML = html);
+      });
+}
